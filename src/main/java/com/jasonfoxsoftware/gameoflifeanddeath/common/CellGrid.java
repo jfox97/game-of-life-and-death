a8f81cbd28e68bf403f2db560d7fa46f373d7bd0
Jason Fox <jfox97@gmail.com>
2018-08-31 20:06:58 -0600

    Write tick logic

--- src/main/java/com/jasonfoxsoftware/gameoflifeanddeath/common/CellGrid.java
+++ src/main/java/com/jasonfoxsoftware/gameoflifeanddeath/common/CellGrid.java
@@ -1,6 +1,8 @@
 package com.jasonfoxsoftware.gameoflifeanddeath.common;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CellGrid
 {
@@ -51,7 +53,33 @@ public class CellGrid
 
     public void tick()
     {
+        for (Map.Entry<String, Cell> entry : aliveCellsCurrent.entrySet())
+        {
+            Cell liveCell = entry.getValue();
 
+            moveLiveCellToNextTick(liveCell);
+            moveDeadNeighborsToNextTick(liveCell);
+        }
+
+        aliveCellsCurrent = aliveCellsNextTick;
+        aliveCellsNextTick = new HashMap<String, Cell>(gridHeight * gridWidth);
+    }
+
+    public Cell[] getLiveCells()
+    {
+        ArrayList<Cell> liveCells = new ArrayList<Cell>();
+        for (Map.Entry<String, Cell> entry : aliveCellsCurrent.entrySet())
+        {
+            liveCells.add(entry.getValue());
+        }
+
+        return liveCells.toArray(new Cell[0]);
+    }
+
+    public void clear()
+    {
+        aliveCellsCurrent.clear();
+        aliveCellsNextTick.clear();
     }
 
     // Adds new alive cell to the "next tick" map
@@ -64,7 +92,7 @@ public class CellGrid
     {
         Cell cell = new Cell(x, y);
 
-        if (x < 0 || y < 0)
+        if (x < 1 || y < 1)
             throw new InvalidCoordinateException();
 
         if (x > gridWidth || y > gridHeight)
@@ -77,7 +105,7 @@ public class CellGrid
         return true;
     }
 
-    private boolean passLivingCellToNextTick(Cell cell)
+    private boolean propogateLiveCell(Cell cell)
     {
         if (aliveCellsCurrent.containsKey(cell.toString()) == false)
             throw new CellNotLivingException(
@@ -108,17 +136,36 @@ public class CellGrid
         return liveNeighborCount;
     }
 
+    // Keeps cell alive if it meets the living conditions
     private void moveLiveCellToNextTick(Cell cell)
     {
         int liveNeighborCount = countLiveNeighbors(cell);
         if (liveNeighborCount > 1 && liveNeighborCount < 4)
-            passLivingCellToNextTick(cell);
+            propogateLiveCell(cell);
     }
 
+    // Makes a dead cell become alive if it meets the conditions
     private void moveDeadCellToNextTick(Cell cell)
     {
         int liveNeighborCount = countLiveNeighbors(cell);
         if (liveNeighborCount == 3)
             birthCellNextTick(cell);
+    }
+
+    /* Iteratively makes each individual neighbor cell to the given cell alive
+       if it meets the proper conditions */
+    private void moveDeadNeighborsToNextTick(Cell cell)
+    {
+        for (int x = cell.x - 1; x <= cell.x + 1; x++)
+        {
+            for (int y = cell.y - 1; y <= cell.y + 1; y++)
+            {
+                if (x == cell.x && y == cell.y)
+                    continue;
+
+                Cell neighborCell = new Cell(x, y);
+                moveDeadCellToNextTick(neighborCell);
+            }
+        }
     }
 }
--- src/test/java/com/jasonfoxsoftware/com/gameoflifeanddeat/common/CellGridTest.java
+++ src/test/java/com/jasonfoxsoftware/com/gameoflifeanddeat/common/CellGridTest.java
@@ -1,6 +1,8 @@
 package com.jasonfoxsoftware.com.gameoflifeanddeat.common;
 
+import com.jasonfoxsoftware.gameoflifeanddeath.common.Cell;
 import com.jasonfoxsoftware.gameoflifeanddeath.common.CellGrid;
+import com.jasonfoxsoftware.gameoflifeanddeath.common.CoordinateOutOfBoundsException;
 import com.jasonfoxsoftware.gameoflifeanddeath.common.InvalidCoordinateException;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
@@ -44,7 +46,99 @@ public class CellGridTest
         CellGrid testGrid = new CellGrid();
 
         exception.expect(InvalidCoordinateException.class);
-        testGrid.setGridDimensions(0, 0);
-        testGrid.setGridDimensions(-1,-1);
+        testGrid.setGridDimensions(0, 5);
+        testGrid.setGridDimensions(5,0);
+    }
+
+    @Test
+    public void birthShouldNotAcceptInvalidCells()
+    {
+        CellGrid testGrid = new CellGrid(10, 10);
+
+        testGrid.birthCell(7, 7);
+        boolean cellAdded = testGrid.birthCell(7, 7);
+
+        assertFalse(cellAdded);
+
+        exception.expect(InvalidCoordinateException.class);
+        testGrid.birthCell(0, 5);
+        testGrid.birthCell(2,0);
+
+        exception.expect(CoordinateOutOfBoundsException.class);
+        testGrid.birthCell(11,4);
+        testGrid.birthCell(3,11);
+    }
+
+    @Test
+    public void clearShouldRemoveAllLiveCellsFromGrid()
+    {
+        CellGrid testGrid = new CellGrid(10, 10);
+
+        testGrid.birthCell(2,5);
+        testGrid.birthCell(6,2);
+        testGrid.birthCell(2,7);
+        testGrid.birthCell(1,1);
+        testGrid.birthCell(10,10);
+
+        testGrid.clear();
+        Cell[] liveCells = testGrid.getLiveCells();
+
+        assertEquals(0, liveCells.length);
+    }
+
+    @Test
+    public void testOscillatingPatterns()
+    {
+        CellGrid testGrid = new CellGrid(5, 5);
+
+        testGrid.birthCell(2, 3);
+        testGrid.birthCell(3, 3);
+        testGrid.birthCell(4, 3);
+
+        testGrid.tick();
+        Cell[] liveCells = testGrid.getLiveCells();
+
+        assertEquals(3, liveCells.length);
+        assertEquals(new Cell(3, 2), liveCells[0]);
+        assertEquals(new Cell(3, 3), liveCells[1]);
+        assertEquals(new Cell(3, 4), liveCells[2]);
+
+        testGrid.tick();
+        liveCells = testGrid.getLiveCells();
+
+        assertEquals(3, liveCells.length);
+        assertEquals(new Cell(4, 3), liveCells[0]);
+        assertEquals(new Cell(2, 3), liveCells[1]);
+        assertEquals(new Cell(3, 3), liveCells[2]);
+
+        testGrid.clear();
+        testGrid = new CellGrid(6,6);
+
+        testGrid.birthCell(2,3);
+        testGrid.birthCell(3,3);
+        testGrid.birthCell(4,3);
+        testGrid.birthCell(3,4);
+        testGrid.birthCell(4,4);
+        testGrid.birthCell(5,4);
+
+        testGrid.tick();
+        liveCells = testGrid.getLiveCells();
+
+        assertEquals(new Cell(2, 3), liveCells[0]);
+        assertEquals(new Cell(3, 2), liveCells[1]);
+        assertEquals(new Cell(5, 3), liveCells[2]);
+        assertEquals(new Cell(4, 5), liveCells[3]);
+        assertEquals(new Cell(2, 4), liveCells[4]);
+        assertEquals(new Cell(5, 4), liveCells[5]);
+
+        testGrid.tick();
+        liveCells = testGrid.getLiveCells();
+
+        assertEquals(new Cell(4, 4), liveCells[0]);
+        assertEquals(new Cell(2, 3), liveCells[1]);
+        assertEquals(new Cell(3, 4), liveCells[2]);
+        assertEquals(new Cell(4, 3), liveCells[3]);
+        assertEquals(new Cell(3, 3), liveCells[4]);
+        assertEquals(new Cell(5, 4), liveCells[5]);
     }
 }
